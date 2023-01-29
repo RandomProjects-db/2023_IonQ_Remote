55caada877a756a5b39416e8e2a05c23285a6e48
Fahed Daibes <61588630+RandomProjects-db@users.noreply.github.com>
2023-01-29 20:43:07 +0200

    Add files via upload

---
+++ test_qiskit.py
@@ -0,0 +1,58 @@
+import qiskit
+from qiskit import quantum_info
+from qiskit.execute_function import execute
+from qiskit import BasicAer
+import numpy as np
+import pickle
+import json
+import os
+from collections import Counter
+from sklearn.metrics import mean_squared_error
+from typing import Dict, List
+import matplotlib.pyplot as plt
+
+#define utility functions
+
+def simulate(circuit: qiskit.QuantumCircuit) -> dict:
+    """Simulate the circuit, give the state vector as the result."""
+    backend = BasicAer.get_backend('statevector_simulator')
+    job = execute(circuit, backend)
+    result = job.result()
+    state_vector = result.get_statevector()
+    
+    histogram = dict()
+    for i in range(len(state_vector)):
+        population = abs(state_vector[i]) ** 2
+        if population > 1e-9:
+            histogram[i] = population
+    
+    return histogram
+
+
+def histogram_to_category(histogram):
+    """This function take a histogram representations of circuit execution results, and process into labels as described in 
+    the problem description."""
+    assert abs(sum(histogram.values())-1)<1e-8
+    positive=0
+    for key in histogram.keys():
+        digits = bin(int(key))[2:].zfill(20)
+        if digits[-1]=='0':
+            positive+=histogram[key]
+        
+    return positive
+
+
+def count_gates(circuit: qiskit.QuantumCircuit) -> Dict[int, int]:
+    """Returns the number of gate operations with each number of qubits."""
+    counter = Counter([len(gate[1]) for gate in circuit.data])
+    #feel free to comment out the following two lines. But make sure you don't have k-qubit gates in your circuit
+    #for k>2
+    for i in range(2,20):
+        assert counter[i]==0
+    return counter
+
+
+def image_mse(image1,image2):
+    # Using sklearns mean squared error:
+    # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mean_squared_error.html
+    return mean_squared_error(255*image1,255*image2)
